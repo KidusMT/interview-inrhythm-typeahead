ba71ab955671decbf8bd13820a6f8820d0721b5d
kevinmongiello <kmong92@gmail.com>
2022-05-06 12:31:17 -0400

    Clean up project.  Remove extra files, consolidate redux files

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-
+// Redux libraries
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+// Root component
 import { FormPage } from './components/form-page/form-page';
-import { configureStore } from './redux';
+// Root reducer
+import { reducer } from './redux';
+
+export const configureStore = () => createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 const store = configureStore();
 
